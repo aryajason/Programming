a04a2832f42923984fdc16f708283e3b2e385636
aryajason <aryajasonr@gmail.com>
2024-10-31 08:35:51 +0700

    Create main.cpp

---
+++ 31 oktober/prac 2/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string name = "matahari" ;
+    name = "zaki" ;
+     cout << name << endl ;
+    
+    return 0;
+}
